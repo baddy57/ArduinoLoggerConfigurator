148d62aefc1976e83d8a4e88f7ab9f83c36ea318
Cristian Baldi <baddy227@gmail.com>
2022-11-03 20:27:26 +0100

    Add project files.

---
+++ src/LoggerConfigurator.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		LoggerConfigurator.cpp
+ Created:	2022-11-03 20:25:06
+ Author:	cristian
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "LoggerConfigurator.h"
+
+
---
+++ src/LoggerConfigurator.h
@@ -0,0 +1,19 @@
+/*
+ Name:		LoggerConfigurator.h
+ Created:	2022-11-03 20:25:06
+ Author:	cristian
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef PLATFORM_LOGGER_HPP_
+#define PLATFORM_LOGGER_HPP_
+
+#include <AVRCircularBufferLogger.h>
+
+#ifndef LOG_BUFFER_SIZE
+#define LOG_BUFFER_SIZE 512
+#endif
+
+using PlatformLogger = PlatformLogger_t<AVRCircularLogBufferLogger<LOG_BUFFER_SIZE>>;
+
+#endif
